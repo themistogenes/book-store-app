ef7c9881c7558f9a5b51842679cf45779a9d3452
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-23 18:14:26 -0400

    setup mongoDB and mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://jalimaras:secret1819@cluster0.c4zsdus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from 'express';
-import { PORT } from './config.js';
-
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+ 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
-})
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
+  app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(200).send('Welcome');
+  })
