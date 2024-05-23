d984891cb41ecc4a269379a291d909cc34111a23
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-23 18:32:40 -0400

    create book

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,42 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
  
 const app = express();
+
+// Middleware for parsing req.body
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(200).send('Welcome');
+})
+
+// Create book
+app.post('/books', async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({
+        message: 'Send all required fields: title, author, publishYear'
+      })
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear
+    }
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({message: error.message});
+  }
+})
 
 mongoose
   .connect(mongoDBURL)
@@ -14,9 +48,4 @@ mongoose
   })
   .catch((error) => {
     console.log(error);
-  })
-
-  app.get('/', (req, res) => {
-    console.log(req);
-    return res.status(200).send('Welcome');
   })
