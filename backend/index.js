a84403a3183d56bf56a9b87a16ef31cb7b4d7377
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-23 19:04:31 -0400

    update book

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,32 @@ app.get('/books/:id', async (req, res) => {
   }
 })
 
+// Update book
+// PUT: /books/:id
+app.put('/books/:id', async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({
+        message: 'Send all required fields: title, author, publishYear'
+      })
+    }
+    const { id } = req.params;
+    const book = await Book.findByIdAndUpdate(id, req.body);
+    if (!book) {
+      return res.status(404).json({message: 'Book not found'});
+    }
+    const updatedBook = await Book.findById(book.id);
+    return res.status(200).json(updatedBook);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({message: error.message});
+  }
+})
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
