7aa1d360d86638076c1f17a323392cfe97dc9556
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-23 18:47:11 -0400

    get single book

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,19 @@ app.get('/books', async (req, res) => {
   }
 })
 
+// Get single book
+// GET: /books/:id
+app.get('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({message: error.message});
+  }
+})
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
