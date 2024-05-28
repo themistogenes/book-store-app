6c07d3e3b9b4b5244def955cf20e1d55b9b80471
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-28 11:26:07 -0400

    handle CORS policy

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,24 @@ import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
  
 const app = express();
 
 // Middleware for parsing req.body
 app.use(express.json());
+
+// Middleware for handling CORS (Cross Origin Resource Sharing) policy
+// Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+// Option 2: Allow custom origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+//   })
+// )
 
 app.get('/', (req, res) => {
   console.log(req);
